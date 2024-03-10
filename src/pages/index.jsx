a32ce8795f6b395515fd61080a2b653c495b3bfe
mako-agawa <mako.agawa@gmail.com>
2024-03-10 17:21:07 +0900

    🎨 試しにクリックイベント記述

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,9 +3,17 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-
+import { useCallback } from "react";
 
 export default function Home() {
+  const foo = 1;
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    // alert(123);
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,6 +21,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
 
       <Footer />
