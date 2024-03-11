c9e0947a6abf36c606911e1097d7a90d68d6d488
mako-agawa <mako.agawa@gmail.com>
2024-03-11 10:29:48 +0900

    ⚡️ useCllback,useEffectの第二引数について

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,21 +7,21 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
-
-  const handleClick = (e) => {
-    setCount((count) => count +1);
-    setCount((count) => count +1);
-  };
+  const handleClick = useCallback(
+    () => {
+      if (count < 10) {
+        setCount((count) => count + 1);
+      }
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
-
     return () => {
       document.body.style.backgroundColor = "";
     };
   }, []);
-
-  console.log(count);
 
   return (
     <div className={styles.container}>
@@ -33,7 +33,6 @@ export default function Home() {
       <h1>{count}</h1>
       <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
-
       <Footer />
     </div>
   );
